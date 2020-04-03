d590ad008b16ae1f13b8e0078852127b0bfb933e
amyskov <55585026+amyskov@users.noreply.github.com>
2020-04-03 16:09:34 +0300

    Update utils/utils.py
    Co-Authored-By: anmyachev <45976948+anmyachev@users.noreply.github.com>

--- utils/utils.py
+++ utils/utils.py
@@ -124,7 +124,7 @@ def compare_dataframes(ibis_dfs, pandas_dfs, sort_cols=["id"], drop_cols=["id"])
         if len(sort_cols):
             ibis_dfs[idx].sort_values(by=sort_cols, axis=0, inplace=True)
         ibis_dfs[idx].reset_index(drop=True, inplace=True)
-        if len(drop_cols) is not 0:
+        if len(drop_cols):
             ibis_dfs[idx].drop(drop_cols, axis=1, inplace=True)
         # prepare pandas part
         pandas_dfs[idx].reset_index(drop=True, inplace=True)
