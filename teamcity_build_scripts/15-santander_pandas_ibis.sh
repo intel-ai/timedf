47fe84f076685bdb624cda55702813a150a08913
Anatoly Myachev <anatoly.myachev@intel.com>
2020-04-14 23:47:22 +0300

    add -fragments_size option into build scripts

--- teamcity_build_scripts/15-santander_pandas_ibis.sh
+++ teamcity_build_scripts/15-santander_pandas_ibis.sh
@@ -3,6 +3,7 @@ python3 run_ibis_tests.py --env_name ${ENV_NAME} --env_check True --python_versi
 "${PWD}"/../ibis/ --executable "${PWD}"/../omniscidb/build/bin/omnisci_server                               \
 -calcite_port 62225 -http_port 1718 -port 1717 -user admin -password HyperInteractive                       \
 -database_name ${DATABASE_NAME} -table santander -bench_name santander -dfiles_num 1 -iterations 5          \
+-fragments_size 2000000                                                                                     \
 -data_file '/localdisk/benchmark_datasets/santander/train.csv.gz'                                           \
 -pandas_mode Pandas -ray_tmpdir /tmp -validation True                                                       \
 -db_server ansatlin07.an.intel.com -db_port 3306 -db_user gashiman -db_pass omniscidb -db_name omniscidb    \
