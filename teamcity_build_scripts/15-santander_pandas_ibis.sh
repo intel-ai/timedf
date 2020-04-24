a9ba186de88953d443cde9ddbd62241c0ea72d8a
Dmitry Chigarev <dmitry.chigarev@intel.com>
2020-04-24 12:11:50 +0300

    removed omnisci ports

--- teamcity_build_scripts/15-santander_pandas_ibis.sh
+++ teamcity_build_scripts/15-santander_pandas_ibis.sh
@@ -1,9 +1,8 @@
 python3 run_ibis_tests.py --env_name ${ENV_NAME} --env_check True --save_env True --python_version 3.7 --task benchmark       \
                           --ci_requirements "${PWD}"/ci_requirements.yml                                                      \
                           --ibis_path "${PWD}"/../ibis/                                                                       \
-                          --executable "${PWD}"/../omniscidb/build/bin/omnisci_server                                         \
+                          -executable "${PWD}"/../omniscidb/build/bin/omnisci_server                                          \
                           -database_name ${DATABASE_NAME} -table santander -bench_name santander -dfiles_num 1 -iterations 5  \
-                          -calcite_port 62225 -http_port 1718 -port 1717 -user admin -password HyperInteractive               \
                           -data_file '/localdisk/benchmark_datasets/santander/train.csv.gz'                                   \
                           -pandas_mode Pandas -ray_tmpdir /tmp -validation True                                               \
                           -commit_omnisci ${BUILD_REVISION} -commit_ibis ${BUILD_IBIS_REVISION}                               \
