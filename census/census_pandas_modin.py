a24e04a06c7f19600360847206ed0f50b300c773
izamyati <igor.zamyatin@intel.com>
2021-11-09 16:28:11 -0600

    Rough attempt to get standalone census

--- census/census_pandas_modin.py
+++ census/census_pandas_modin.py
@@ -1,23 +1,183 @@
 # coding: utf-8
+import os
+import argparse
 import numpy as np
-from sklearn import config_context
+#from sklearn import config_context
 import warnings
 from timeit import default_timer as timer
 
-from utils import (
-    check_support,
-    cod,
-    import_pandas_into_module_namespace,
-    load_data_pandas,
-    load_data_modin_on_omnisci,
-    mse,
-    print_results,
-    split,
-    getsize,
-)
+#from utils import (
+#    check_support,
+#    cod,
+#    import_pandas_into_module_namespace,
+#    load_data_pandas,
+#    load_data_modin_on_omnisci,
+#    mse,
+#    print_results,
+#    split,
+#    getsize,
+#)
 
 warnings.filterwarnings("ignore")
 
+conversions = {"ms": 1000, "s": 1, "m": 1 / 60, "": 1}
+
+def convert_units(dict_to_convert, ignore_fields, unit="ms"):
+    try:
+        multiplier = conversions[unit]
+    except KeyError:
+        raise ValueError(f"Conversion to {unit} is not implemented")
+
+    return {
+        key: (value * multiplier if key not in ignore_fields else value)
+        for key, value in dict_to_convert.items()
+    }
+
+def getsize(filename: str):
+    """Return size of filename in MB"""
+    if "://" in filename:
+        if no_deps_mode:
+            raise RuntimeError(f"Size of '{filename}' can not be measured in no-deps mode")
+        if s3_client.s3like(filename):
+            return s3_client.getsize(filename) / 1024 / 1024
+        raise ValueError(f"bad s3like link: {filename}")
+    else:
+        return os.path.getsize(filename) / 1024 / 1024
+
+
+def init_modin_on_omnisci(pd):
+    # Calcite initialization
+    data = {"a": [1, 2, 3]}
+    df = pd.DataFrame(data)
+    df = df + 1
+    _ = df.index
+
+
+def import_pandas_into_module_namespace(namespace, mode, ray_tmpdir=None, ray_memory=None):
+    if mode == "Pandas":
+        print("Pandas backend: pure Pandas")
+        import pandas as pd
+    else:
+        if mode == "Modin_on_ray":
+            import ray
+
+            if not ray_tmpdir:
+                ray_tmpdir = "/tmp"
+            if not ray_memory:
+                ray_memory = 200 * 1024 * 1024 * 1024
+            if not ray.is_initialized():
+                ray.init(
+                    include_dashboard=False,
+                    _plasma_directory=ray_tmpdir,
+                    _memory=ray_memory,
+                    object_store_memory=ray_memory,
+                )
+            os.environ["MODIN_ENGINE"] = "ray"
+            print(
+                f"Pandas backend: Modin on Ray with tmp directory {ray_tmpdir} and memory {ray_memory}"
+            )
+        elif mode == "Modin_on_dask":
+            os.environ["MODIN_ENGINE"] = "dask"
+            print("Pandas backend: Modin on Dask")
+        elif mode == "Modin_on_python":
+            os.environ["MODIN_ENGINE"] = "python"
+            print("Pandas backend: Modin on pure Python")
+        elif mode == "Modin_on_omnisci":
+            os.environ["MODIN_ENGINE"] = "native"
+            os.environ["MODIN_STORAGE_FORMAT"] = "omnisci"
+            os.environ["MODIN_EXPERIMENTAL"] = "True"
+            print("Pandas backend: Modin on OmniSci")
+        else:
+            raise ValueError(f"Unknown pandas mode {mode}")
+        import modin.pandas as pd
+
+        # Some components of Modin with OmniSci engine are initialized only
+        # at the moment of query execution, so for proper benchmarks performance
+        # measurement we need to initialize these parts before any measurements
+        if mode == "Modin_on_omnisci":
+            init_modin_on_omnisci(pd)
+    if not isinstance(namespace, (list, tuple)):
+        namespace = [namespace]
+    for space in namespace:
+        space["pd"] = pd
+
+def print_results(results, backend=None, unit="", ignore_fields=[]):
+    results_converted = convert_units(results, ignore_fields=[], unit=unit)
+    if backend:
+        print(f"{backend} results:")
+    for result_name, result in results_converted.items():
+        if result_name not in ignore_fields:
+            print("    {} = {:.3f} {}".format(result_name, result, unit))
+
+def load_data_pandas(
+    filename,
+    columns_names=None,
+    columns_types=None,
+    header=None,
+    nrows=None,
+    use_gzip=False,
+    parse_dates=None,
+    pd=None,
+    pandas_mode="Pandas",
+):
+    if not pd:
+        import_pandas_into_module_namespace(
+            namespace=load_data_pandas.__globals__, mode=pandas_mode
+        )
+    types = None
+    if columns_types:
+        types = {columns_names[i]: columns_types[i] for i in range(len(columns_names))}
+    return pd.read_csv(
+        filename,
+        names=columns_names,
+        nrows=nrows,
+        header=header,
+        dtype=types,
+        compression="gzip" if use_gzip else None,
+        parse_dates=parse_dates,
+    )
+
+def mse(y_test, y_pred):
+    return ((y_test - y_pred) ** 2).mean()
+
+
+def cod(y_test, y_pred):
+    y_bar = y_test.mean()
+    total = ((y_test - y_bar) ** 2).sum()
+    residuals = ((y_test - y_pred) ** 2).sum()
+    return 1 - (residuals / total)
+
+
+def load_data_modin_on_omnisci(
+    filename, columns_names=None, columns_types=None, parse_dates=None, pd=None, skiprows=None
+):
+    if not pd:
+        import_pandas_into_module_namespace(
+            namespace=load_data_pandas.__globals__, mode="Modin_on_omnisci"
+        )
+    dtypes = None
+    if columns_types:
+        dtypes = {
+            columns_names[i]: columns_types[i] if (columns_types[i] != "category") else "string"
+            for i in range(len(columns_names))
+        }
+
+    all_but_dates = dtypes
+    dates_only = False
+    if parse_dates:
+        parse_dates = parse_dates if isinstance(parse_dates, (list, tuple)) else [parse_dates]
+        all_but_dates = {
+            col: valtype for (col, valtype) in dtypes.items() if valtype not in parse_dates
+        }
+        dates_only = [col for (col, valtype) in dtypes.items() if valtype in parse_dates]
+
+    return pd.read_csv(
+        filename,
+        names=columns_names,
+        dtype=all_but_dates,
+        parse_dates=dates_only,
+        skiprows=skiprows,
+    )
 
 # Dataset link
 # https://rapidsai-data.s3.us-east-2.amazonaws.com/datasets/ipums_education2income_1970-2010.csv.gz
@@ -162,11 +322,11 @@ def ml(X, y, random_state, n_runs, test_size, optimizer, ml_keys, ml_score_keys)
 
 
 def run_benchmark(parameters):
-    check_support(parameters, unsupported_params=["dfiles_num", "gpu_memory"])
+    #check_support(parameters, unsupported_params=["dfiles_num", "gpu_memory"])
 
     parameters["data_file"] = parameters["data_file"].replace("'", "")
-    parameters["optimizer"] = parameters["optimizer"] or "intel"
-    parameters["no_ml"] = parameters["no_ml"] or False
+    #parameters["optimizer"] = parameters["optimizer"] or "intel"
+    #parameters["no_ml"] = parameters["no_ml"] or False
 
     # ML specific
     N_RUNS = 50
@@ -275,8 +435,8 @@ def run_benchmark(parameters):
     import_pandas_into_module_namespace(
         namespace=run_benchmark.__globals__,
         mode=parameters["pandas_mode"],
-        ray_tmpdir=parameters["ray_tmpdir"],
-        ray_memory=parameters["ray_memory"],
+        #ray_tmpdir=parameters["ray_tmpdir"],
+        #ray_memory=parameters["ray_memory"],
     )
 
     etl_times = None
@@ -303,20 +463,55 @@ def run_benchmark(parameters):
     etl_times["Backend"] = parameters["pandas_mode"]
     etl_times["dataset_size"] = csv_size
 
-    if not parameters["no_ml"]:
-        ml_scores, ml_times = ml(
-            X=X,
-            y=y,
-            random_state=RANDOM_STATE,
-            n_runs=N_RUNS,
-            test_size=TEST_SIZE,
-            optimizer=parameters["optimizer"],
-            ml_keys=ml_keys,
-            ml_score_keys=ml_score_keys,
-        )
-        print_results(results=ml_times, backend=parameters["pandas_mode"], unit="s")
-        ml_times["Backend"] = parameters["pandas_mode"]
-        print_results(results=ml_scores, backend=parameters["pandas_mode"])
-        ml_scores["Backend"] = parameters["pandas_mode"]
+#    if not parameters["no_ml"]:
+#        ml_scores, ml_times = ml(
+#            X=X,
+#            y=y,
+#            random_state=RANDOM_STATE,
+#            n_runs=N_RUNS,
+#            test_size=TEST_SIZE,
+#            optimizer=parameters["optimizer"],
+#            ml_keys=ml_keys,
+#            ml_score_keys=ml_score_keys,
+#        )
+#        print_results(results=ml_times, backend=parameters["pandas_mode"], unit="s")
+#        ml_times["Backend"] = parameters["pandas_mode"]
+#        print_results(results=ml_scores, backend=parameters["pandas_mode"])
+#        ml_scores["Backend"] = parameters["pandas_mode"]
 
-    return {"ETL": [etl_times], "ML": [ml_times]}
+ #   return {"ETL": [etl_times], "ML": [ml_times]}
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Run Census Modin perf testing")
+    required = parser.add_argument_group("required arguments")
+
+    required.add_argument(
+        "-data_file", dest="data_file", help="A datafile that should be loaded.", required=True
+    )
+
+    required.add_argument(
+        "-pandas_mode",
+        choices=["Pandas", "Modin_on_ray", "Modin_on_dask", "Modin_on_python", "Modin_on_omnisci"],
+        default="Pandas",
+        help="Specifies which version of Pandas to use: plain Pandas, Modin runing on Ray or on Dask or on Omnisci",
+    )
+
+    args = parser.parse_args()
+    args_dict = vars(args)
+    args_dict = {
+        key: os.path.expandvars(value) if isinstance(value, str) else value
+        for key, value in args_dict.items()
+    }
+
+    parameters = {
+        "data_file": args_dict["data_file"],
+        "pandas_mode": args_dict["pandas_mode"],
+    }
+
+    run_benchmark(parameters)
+
+
+if __name__ == "__main__":
+    main()
