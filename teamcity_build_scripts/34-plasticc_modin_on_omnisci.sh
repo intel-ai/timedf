1e7924a99dfc90ab3c1f439aaab73a9e80bbfaed
Anatoly Myachev <anatoly.myachev@intel.com>
2022-01-17 14:23:36 +0300

    add comments
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- teamcity_build_scripts/34-plasticc_modin_on_omnisci.sh
+++ teamcity_build_scripts/34-plasticc_modin_on_omnisci.sh
@@ -2,6 +2,8 @@
 
 source ${CONDA_PREFIX}/bin/activate
 conda activate ${ENV_NAME}
+# This variable is used to improve performance and its value was obtained during the experiment.
+# Each workload can have a different value.
 export MODIN_OMNISCI_FRAGMENT_SIZE=32000000
 mkdir -p ${PWD}/tmp
 python3 run_modin_tests.py -bench_name plasticc -data_file "${DATASETS_PWD}/plasticc/"                                             \
