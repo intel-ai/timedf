285c0caf654dd4f08fc0cf6e3fdc509ba898c72c
Egor Krivov <e.a.krivov@gmail.com>
2022-12-01 22:47:46 +0100

    Update load_data.sh

--- docker/e2e-hdk/load_data.sh
+++ docker/e2e-hdk/load_data.sh
@@ -7,6 +7,6 @@ USER_ID="$(id -u):$(id -g)"
 # awsd="docker run --rm -it -e http_proxy=${http_proxy} -e https_proxy=${https_proxy} --user ${USER_ID} -v ${DATASETS_ROOT}:/datasets amazon/aws-cli s3"
 awsd="docker run --rm -it  --user ${USER_ID} -v ${DATASETS_ROOT}:/datasets amazon/aws-cli s3"
 
-$awsd sync s3://modin-datasets/taxi /datasets/taxi --no-sign-request --exclude "*" --include "trips_xa{a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t}.csv"
+$awsd sync s3://modin-datasets/taxi /datasets/taxi --no-sign-request --exclude "*" --include "trips_xa[abcdefghijklmnopqrst].csv"
 $awsd sync s3://modin-datasets/plasticc /datasets/plasticc --no-sign-request --exclude "*" --include "*.csv"
 $awsd sync s3://modin-datasets/census /datasets/census --no-sign-request
