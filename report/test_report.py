980d60f2c524d2841cf9ce0749fd20193822d153
Egor Krivov <e.a.krivov@gmail.com>
2022-12-28 09:00:20 -0600

    added test for dbreport

---
+++ report/test_report.py
@@ -0,0 +1,24 @@
+import pytest
+
+from sqlalchemy import create_engine, select
+from report.report import DbReport
+
+
+@pytest.fixture(scope="session")
+def engine():
+    return create_engine("sqlite://", future=True)
+
+
+def test_dbreport(engine):
+    """Returns an sqlalchemy session, and after the test tears down everything properly."""
+    table_name = 'tablename'
+    report = DbReport(engine, table_name, ['result'], {'setting1': 'param1'})
+
+    report.submit({'result': 12})
+    
+    stmt = select(report._table)
+
+    with engine.connect() as conn:
+        results = list(conn.execute(stmt))
+        assert len(results)
+        assert results[0]['result'] == '12'
