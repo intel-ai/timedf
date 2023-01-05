ca5e01b98247887f30141231b73075ace0e07a9e
Egor Krivov <e.a.krivov@gmail.com>
2023-01-05 08:51:19 -0600

    fixed raise error in h2o

--- h2o/h2o_modin.py
+++ h2o/h2o_modin.py
@@ -541,7 +541,6 @@ def queries_modin(filename, pandas_mode, extended_functionality):
         print(f"{pandas_mode} {query_name} results:")
         print_results(results=queries_results[query_name], unit="s")
 
-    raise ValueError(f"Need to transorm queris_results {queries_results} into a flat form")
     return queries_results, {f"dataset_size_{name}": val for name, val in query_data_file_sizes}
 
 
