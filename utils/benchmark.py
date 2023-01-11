c5673a1bf24f19dab7fc6ddc7984d5e63349073e
Egor Krivov <e.a.krivov@gmail.com>
2023-01-11 10:54:19 -0600

    allowed benchmark parameter conversion to str

--- utils/benchmark.py
+++ utils/benchmark.py
@@ -15,22 +15,33 @@ class BenchmarkResults:
             Additinal parameters of the current benchmark that need to be saved as well.
             Example: `{'dataset_size': 122, 'dfiles_n': 99}`
         """
-        self._validate_dict(measurements, float)
+        self._validate_dict(measurements)
+        self._validate_vals(measurements, float)
         self.measurements = measurements
-        self._validate_dict(params or {}, str)
-        self.params = params
+        self._validate_dict(params or {})
+        self.params = self._convert_vals(params, str)
+
 
     @staticmethod
-    def _validate_dict(measurements, val_type):
-        if not isinstance(measurements, dict):
+    def _validate_dict(res):
+        if not isinstance(res, dict):
             raise ValueError(
-                f"Measurements have to be of dict type, but they are {type(measurements)}"
+                f"Measurements have to be of dict type, but they are {type(res)}"
             )
 
-        for key, val in measurements.items():
+
+    @staticmethod
+    def _validate_vals(res, val_type):
+        for key, val in res.items():
             if not isinstance(val, val_type):
                 raise ValueError(f'Value for key="{key} is not {val_type}! type={type(val)}"')
 
+    @staticmethod
+    def _convert_vals(res, val_type):
+        if res is None:
+            return None
+        else:
+            return {k: val_type(v) for k, v in res.items()}
 
 class BaseBenchmark(abc.ABC):
     def run(self, params) -> BenchmarkResults:
