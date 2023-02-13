426809fa29b7b56b998cb8ffa1025d449646e169
Egor Krivov <e.a.krivov@gmail.com>
2023-02-13 11:32:45 -0600

    reverted broken HDK

--- docker/e2e-hdk/run_benchmarks.sh
+++ docker/e2e-hdk/run_benchmarks.sh
@@ -5,18 +5,18 @@ export ADDITIONAL_OPTS="-iterations 3"
 
 # Modin_on_hdk
 # !!!This optimization is currently broken!!!
-# export LD_PRELOAD_OLD=${LD_PRELOAD}
-# export LD_LIBRARY_PATH_OLD=${LD_LIBRARY_PATH}
+export LD_PRELOAD_OLD=${LD_PRELOAD}
+export LD_LIBRARY_PATH_OLD=${LD_LIBRARY_PATH}
 
-# export LD_PRELOAD=${CONDA_PREFIX}/envs/${ENV_NAME}/lib/libtbbmalloc_proxy.so.2
-# export LD_LIBRARY_PATH=${CONDA_PREFIX}/envs/${ENV_NAME}/lib/
+export LD_PRELOAD=${CONDA_PREFIX}/envs/${ENV_NAME}/lib/libtbbmalloc_proxy.so.2
+export LD_LIBRARY_PATH=${CONDA_PREFIX}/envs/${ENV_NAME}/lib/
 
 ./teamcity_build_scripts/33-ny_taxi_modin_on_hdk_20M_records.sh |& tee /results/taxi_hdk.res
 ./teamcity_build_scripts/30-census_modin_on_hdk.sh |& tee /results/census_hdk.res
 ./teamcity_build_scripts/34-plasticc_modin_on_hdk.sh |& tee /results/plasticc_hdk.res
 
-# export LD_PRELOAD=${LD_PRELOAD_OLD}
-# export LD_LIBRARY_PATH=${LD_LIBRARY_PATH_OLD}
+export LD_PRELOAD=${LD_PRELOAD_OLD}
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH_OLD}
 
 # Stock pandas
 # HDK scripts deactivate conda, so we need to reactivate it again
