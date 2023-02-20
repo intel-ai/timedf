3627211f07ee80c3502dd61427e9ca13ac4d16b3
Egor Krivov <e.a.krivov@gmail.com>
2023-02-20 08:44:43 -0600

    Fixed flake8 warning

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -6,7 +6,7 @@ from pathlib import Path
 def create_benchmark(bench_name):
     try:
         return importlib.import_module(f'.{bench_name}', __name__).Benchmark()
-    except ModuleNotFoundError as e:
+    except ModuleNotFoundError:
         available_benchmarks = [p.name for p in Path(__name__).iterdir() if p.is_dir()]
         raise ValueError(
             f'Attempted to create benchmark "{bench_name}", but it is missing from '
