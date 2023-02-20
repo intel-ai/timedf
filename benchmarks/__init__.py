5e5cf85d3e8ec61fe0542ae4556e09717563b8f9
Egor Krivov <e.a.krivov@gmail.com>
2023-02-20 09:19:28 -0600

    Fixed extra space

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -10,5 +10,5 @@ def create_benchmark(bench_name):
         available_benchmarks = [p.name for p in Path(__name__).iterdir() if p.is_dir()]
         raise ValueError(
             f'Attempted to create benchmark "{bench_name}", but it is missing from '
-            f' the list of available benchmarkrs, which contains: "{available_benchmarks}"'
+            f'the list of available benchmarkrs, which contains: "{available_benchmarks}"'
         )
