a6ca3639ed65d911e919c303a4e5f78cefe20311
Alexei Fedotov <alexei.fedotov@gmail.com>
2023-02-21 00:12:31 +0100

    Add 25 ray perf functions

---
+++ .github/workflows/modin/asv-ray-runner.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -vxe
+cd /_work
+
+# HDK
+(
+    mkdir hdk
+    cd hdk
+    tar --strip-components=1 -zxf /_work/release.tgz -C $CONDA_PREFIX install-prefix
+)
+
+# Modin installation
+(
+    git clone https://github.com/modin-project/modin.git
+    cd modin && pip install -e .[ray]
+
+    git apply /_work/omniscripts/.github/workflows/modin/groupby.patch
+)
+
+# ASV installation
+(
+    git clone https://github.com/airspeed-velocity/asv.git
+    cd asv
+    git checkout ef016e233cb9a0b19d517135104f49e0a3c380e9
+    git apply ../omniscripts/docker/microbenchmarks-hdk/asv-default-timeout.patch
+    cd ..
+    cd asv && pip install -e .
+)
+
+echo "asv script launch"
+export ENV_NAME=modin-test
+export HOST_NAME=c9n7
+export MODIN_USE_CALCITE=True
+
+bash -vx ./omniscripts/docker/microbenchmarks-hdk/asv-runner.sh
+
