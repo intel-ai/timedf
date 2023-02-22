17e370226edc1e5d2f0a356561627c157d0c7c33
Egor Krivov <e.a.krivov@gmail.com>
2023-02-22 12:17:52 -0600

    removed accidental file

--- build_scripts/env.sh
+++
@@ -1,6 +0,0 @@
-export ENV_NAME=hm
-export DATASETS_PWD=/localdisk/benchmark_datasets
-# export DB_COMMON_OPTS="-db_server ansatlin07.an.intel.com -db_port 3306 -db_user gashiman -db_pass omniscidb -db_name omniscidb"
-# export DB_COMMON_OPTS="-db_name database_test.sqlite"
-export DB_COMMON_OPTS="-db_server ansatlin07.an.intel.com -db_port 3306 -db_user gashiman -db_pass omniscidb -db_name omniscidb -db_driver mysql+mysqlconnector"
-export ADDITIONAL_OPTS="-iterations 1"
