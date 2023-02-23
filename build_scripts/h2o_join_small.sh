36a4f9290d247a8144c16bdc0fbb9f8ab1948776
Egor Krivov <e.a.krivov@gmail.com>
2023-02-23 06:22:11 -0600

    Updated plasticc

--- build_scripts/h2o_join_small.sh
+++ build_scripts/h2o_join_small.sh
@@ -3,11 +3,4 @@
 BENCH_NAME="h2o"
 DATA_FILE="${DATASETS_PWD}/h2o/J1_1e7_NA_0_0.csv" 
 
-python3 run_modin_tests.py -task benchmark                                   \
-                           -bench_name h2o                                   \
-                           -data_file "${DATA_FILE}"                         \
-                           -pandas_mode "${PANDAS_MODE}"                     \
-                           -ray_tmpdir ${PWD}/tmp                            \
-                           ${ADDITIONAL_OPTS}                                \
-                           ${ADDITIONAL_OPTS_NIGHTLY}                        \
-                           ${DB_COMMON_OPTS}
+source $(dirname "$0")/00-run_bench.sh
