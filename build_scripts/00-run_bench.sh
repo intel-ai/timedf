1f1c66df8084ec899354b2331b820da3059c3381
Egor Krivov <e.a.krivov@gmail.com>
2023-02-27 04:17:21 -0600

    Fixed some bugs

--- build_scripts/00-run_bench.sh
+++ build_scripts/00-run_bench.sh
@@ -2,8 +2,12 @@
 
 mkdir -p ${PWD}/tmp
 
+# ENV_NAME must be defined
+if [[ -z "${ENV_NAME}" ]]; then
+  echo "Please, provide ENV_NAME environment variable"
+  exit 1
+fi
 
-# ENV_NAME must be provided
 if [[ -z "${PANDAS_MODE}" ]]; then
   echo "Please, provide PANDAS_MODE environment variable" 
   exit 1
@@ -12,7 +16,11 @@ else
 fi
 
 # Run benchmark
-conda run -n $ENV_NAME python3 run_modin_tests.py -task benchmark \
+
+# ENV_NAME must be provided
+# live stream will provide live stdout and stderr
+conda run --live-stream -n $ENV_NAME python3 run_modin_tests.py   \
+                           -task benchmark                        \
                            -bench_name $BENCH_NAME                \
                            -data_file "${DATA_FILE}"              \
                            -pandas_mode ${PANDAS_MODE}            \
--- build_scripts/plasticc.sh
+++ build_scripts/plasticc.sh
@@ -8,4 +8,5 @@ BENCH_NAME="plasticc"
 DATA_FILE="${DATASETS_PWD}/plasticc/"
 
 # This benchmark also support -use_xgb True
+USE_MODIN_XGB="False"
 source $(dirname "$0")/00-run_bench.sh -use_modin_xgb "${USE_MODIN_XGB}"
