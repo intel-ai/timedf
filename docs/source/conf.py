de852a185f96cc844bbe50f74c25bffc08726beb
Egor Krivov <e.a.krivov@gmail.com>
2023-03-01 11:31:09 -0600

    fixed codestyle

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,24 +5,23 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-project = 'omniscripts'
-copyright = '2023, Omniscripts developers'
-author = 'Omniscript developers'
+project = "omniscripts"
+copyright = "2023, Omniscripts developers"
+author = "Omniscript developers"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-]
+extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['build']
+templates_path = ["_templates"]
+exclude_patterns = ["build"]
 
-root_doc = 'index'
+root_doc = "index"
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
