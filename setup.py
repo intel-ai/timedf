9d3b3fdf14c9871f6cf85eacc0580b9aee21ed8e
Egor Krivov <e.a.krivov@gmail.com>
2023-03-30 06:45:04 -0500

    Fixes

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from pathlib import Path
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-reqs_dir = Path(__file__).parents[0] / "requirements"
+root = Path(__file__).resolve().parent
+
+with open(root / "README.md", encoding="utf-8") as f:
+    long_description = f.read()
 
 
 def parse_reqs(name):
-    with open(reqs_dir / name, "r") as f:
+    with open(root / "requirements" / name, "r") as f:
         return f.readlines()
 
 
@@ -14,14 +17,17 @@ dev_reqs = parse_reqs("linters.txt") + parse_reqs("unittests.txt")
 
 all_reqs = reporting_reqs + dev_reqs
 
+
 setup(
     name="omniscripts",
     version="0.1",
     description="Tools for benchmarking data processing with data frames",
+    long_description=long_description,
     author="???",
     author_email="???",
     url="https://github.com/intel-ai/omniscripts/",
-    packages=["omniscripts"],
+    include_package_data=True,
+    packages=find_packages(exclude=["scripts", "scripts.*"]),
     # I suggest we migrate from requirements.yaml to requirements.txt and just put these file here:
     install_requires=parse_reqs("base.txt"),
     extras_require={"reporting": reporting_reqs, "dev": dev_reqs, "all": all_reqs},
