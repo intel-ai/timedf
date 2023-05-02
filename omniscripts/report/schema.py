d7aa9ec7b69cca7e4a511308ab9f6013b2dcb52a
Egor Krivov <e.a.krivov@gmail.com>
2023-05-02 07:30:40 -0500

    reverted schema change

--- omniscripts/report/schema.py
+++ omniscripts/report/schema.py
@@ -18,7 +18,6 @@ Base = declarative_base()
 
 
 STRING_LENGTH = 200
-LARGE_STRING_LENGTH = 500
 
 
 def make_string(nullable=False):
@@ -56,7 +55,7 @@ class Measurement(Base):
     __tablename__ = "measurement"
     id = Column(Integer, primary_key=True)
     # Name of the measurement
-    name = Column(String(LARGE_STRING_LENGTH), nullable=False)
+    name = Column(String(STRING_LENGTH), nullable=False)
     # Duration in seconds
     duration_s = Column(Float, nullable=False)
 
