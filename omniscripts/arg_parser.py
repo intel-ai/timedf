e2637a1863e720127430ef23a9615fb5d712d6a6
Egor Krivov <e.a.krivov@gmail.com>
2023-05-11 09:10:01 -0500

    renamed function as well

--- omniscripts/arg_parser.py
+++ omniscripts/arg_parser.py
@@ -25,7 +25,7 @@ class DbConfig:
     password: str = None
     name: str = None
 
-    def is_config_complete(self):
+    def is_config_available(self):
         return self.name is not None
 
     def _create_engine(self):
@@ -43,7 +43,7 @@ class DbConfig:
         return create_engine(url)
 
     def maybeCreateBenchmarkDb(self):
-        if self.is_config_complete():
+        if self.is_config_available():
             from .report import BenchmarkDb
 
             return BenchmarkDb(self._create_engine())
