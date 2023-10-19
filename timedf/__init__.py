7abc0c91b54dc22451dbcb8910c5d3963f398090
Egor Krivov <egor.krivov@intel.com>
2023-10-19 13:24:06 +0000

    Removed backend

--- timedf/__init__.py
+++ timedf/__init__.py
@@ -1,6 +1,5 @@
 from .benchmark import BaseBenchmark, BenchmarkResults
 from .timer import TimerManager, tm
 from . import benchmark_utils
-from .backend import Backend
 
 from .__version__ import __version__
